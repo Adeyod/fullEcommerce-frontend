38557071bcaee37f07d915e332a74a00b0657a76
Ayodeji Adebolu <ayodejiadebolu@gmail.com>
2023-10-12 18:05:28 +0100

    new push

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), pluginRewriteAll()],
+
+  // added
+  build: {
+    // Specify your entry point
+    rollupOptions: {
+      input: '/src/main.jsx', // Replace with the actual path to your entry file
+    },
+  },
 });
